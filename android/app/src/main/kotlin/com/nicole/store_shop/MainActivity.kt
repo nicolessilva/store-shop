316d73ca468ab9136d90bd0c10eac6fee922932f
nicolessilva <nicolesilvaon@gmail.com>
2022-10-12 11:05:33 -0300

    feat: add Firebase

--- android/app/src/main/kotlin/com/nicole/store_shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/nicole/store_shop/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.store_shop
+package com.nicole.store_shop
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -113,9 +113,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/nicolessilva/store-shop.git
-git push -u origin main
